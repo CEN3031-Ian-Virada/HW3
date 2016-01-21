ba4befe1aec4a8aa0f90c997561faf67bdd305dd
Virada Long <viradalong16@ufl.edu>
2016-01-20 20:32:06 -0500

    fixed connect

--- queries.js
+++ queries.js
@@ -5,25 +5,25 @@ var mongoose = require('mongoose'),
     Listing = require('./ListingSchema.js'),
     config = require('./config.js');
 
-mongoose.connect(config.uri);
+mongoose.connect(config.db.uri);
 
 
 var findLibraryWest = function() {
-  /* 
+  /*
     Find the document that contains data corresponding to Library West,
-    then log it to the console. 
+    then log it to the console.
    */
-   Listing.find({ code: 'LBW' }, function(err) {
+   Listing.find({ code: 'LBW' }, function(err, listing) {
     if (err) throw err;
 
-    console.log(user)
+    console.log(listing);
    });
 };
 var removeCable = function() {
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    and remove this listing from your database and log the document to the console.
    */
    Listing.find({ code: 'CABL' }, function(err, listing) {
      if(err) throw err;
@@ -31,14 +31,14 @@ var removeCable = function() {
      listing.remove(function(err) {
        if(err) throw err;
 
-       console.log(listing);
+       console.log('Listing deleted.');
      });
    });
 };
 var updatePhelpsMemorial = function() {
   /*
-    Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
+    Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then
+    log the updated document to the console.
    */
    Listing.find({ code: 'PHL' }, function(err, listing){
      if(err) throw err;
@@ -48,8 +48,8 @@ var updatePhelpsMemorial = function() {
    });
 };
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
+  /*
+    Retrieve all listings in the database, and log them to the console.
    */
    Listing.find({}, function(err, listing) {
     if(err) throw err;
